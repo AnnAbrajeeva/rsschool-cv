07f1d136928254402a3f7246861f221d5ca05a41
Ann Abrajeeva <anuta040786@yandex.ru>
2021-12-17 21:42:21 +0500

    feat: add info about english and projects

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 hljs.highlightAll();
+
+const burger = document.querySelector('.burger-menu');
+const menu = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('open')
+    menu.classList.toggle('active')
+
+    if(burger.classList.contains('open')) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow = ''
+    }
+})
